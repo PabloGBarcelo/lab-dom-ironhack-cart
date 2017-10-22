1c03547a65d21867aad57db640ea45aa576f1892
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-10-22 22:36:56 +0200

    Added autorefresh and fix styles

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,7 +1,3 @@
-function deleteItem(e){
-
-}
-
 function getPriceByProduct(itemNode){
   var totalPrices = [];
   var index= [];
@@ -48,21 +44,126 @@ function createNewItem(divCopied) {
   divCopied.querySelector(".btn-delete").onclick= function(){
     this.parentNode.parentNode.parentNode.remove();
   };
-  console.log(divCopied);
+  divCopied.querySelector(".qty").onchange=function(){
+    detectChangeQuantity();
+  };
   document.querySelector(".content").appendChild(divCopied);
+}
+
+function detectChangeQuantity(){
+  var checkValue = document.querySelector('.qty').value;
+  console.log(checkValue);
+  if (checkValue < 0 || !/^\d+$/.test(checkValue)){
+    document.querySelector('.qty').value = 1;
+    alert("Please, insert a value");
+  }
+  else if(checkValue == 0){
+    if (confirm('Are you sure you want to remove this item from your cart?')) {
+    // Remove it!
+    this.parentNode.parentNode.parentNode.remove();
+    } else {
+    // Do nothing!
+    document.querySelector('.qty').value = 1;
+    }
+  }
+  else{
+    if (document.querySelector('.refreshOrNot').checked == true){
+      getTotalPrice();
+    }
+  }
 }
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-  var setCopy = document.querySelector(".info").cloneNode(true);
+  var quantityBox = document.getElementsByClassName('qty');
+  var autoRefreshEnabled = document.querySelector('.refreshOrNot');
+  var setCopy, buttonRefresh;
+  // Initial Price
+  getTotalPrice();
   calculatePriceButton.onclick = getTotalPrice;
   // Anonymous function for not auto execute
   createItemButton.onclick = function(){
-    setCopy = document.querySelector(".info").cloneNode(true);
-    createNewItem(setCopy);
+    // cloneNode(true) Childs copy too
+    var valueEmpty = document.querySelector(".newItem");
+    var priceEmpty = document.querySelector(".priceNewItem");
+    if (valueEmpty.value == ""){
+      alert("Please, fill item product");
+    }else{
+      setCopy = document.querySelector(".info").cloneNode(true);
+      if (priceEmpty.value == ""){
+        priceEmpty.value = 0;
+      }
+      createNewItem(setCopy);
+      getTotalPrice();
+    }
+  };
+  autoRefreshEnabled.onchange = function(){
+    buttonRefresh = document.querySelector(".btn-success");
+    if (document.querySelector('.refreshOrNot').checked == true){
+      buttonRefresh.disabled = true;
+      buttonRefresh.style.backgroundColor = "#cacaca";
+      buttonRefresh.style.borderColor = "#ffffff";
+    }
+    else{
+      buttonRefresh.disabled = false;
+      buttonRefresh.style.backgroundColor = "#5cb85c";
+      buttonRefresh.style.borderColor = "#4cae4c";
+    }
   };
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+  for(var z = 0; z<quantityBox.length; z++){
+    quantityBox[z].onchange = detectChangeQuantity;
+  }
 };
+
+// external js
+// https://draggabilly.desandro.com/draggabilly.pkgd.js
+
+// use left/top for all positioning
+Draggabilly.prototype.positionDrag = Draggabilly.prototype.setLeftTop;
+
+$( function() {
+  $('.knob').each( function( i, knob ) {
+    var draggie = new Draggabilly( knob, {
+      containment: true,
+      axis: 'x'
+    });
+    draggie.on( 'dragMove', onDragMove );
+    draggie.on( 'dragEnd', onDragEnd );
+
+  });
+});
+
+function onDragMove() {
+  var $knob = $( this.element );
+  var $label = $knob.parents('label');
+  // on initial drag move
+  if ( !this.hasDragged ) {
+    this.movePotential = $knob.parent().width() - $knob.width();
+    $label.on( 'click', onLabelClickDragging );
+    this.input = $label.find('input')[0];
+    this.hasDragged = true;
+  }
+
+  var isChecked = this.position.x > this.movePotential / 2;
+  this.input.checked = isChecked;
+}
+
+// disable label clicks when dragging
+function onLabelClickDragging( event ) {
+  event.preventDefault();
+}
+
+function onDragEnd() {
+  // clear out Draggabilly's position setting
+  this.element.style.left = '';
+  delete this.hasDragged;
+  var $knob = $( this.element );
+  var $label = $knob.parents('label');
+  setTimeout( function() {
+    $label.off( 'click', onLabelClickDragging );
+  });
+}
